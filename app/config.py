ec53c185533461c7a3634339a033088a2e080811
dxncat <xidarkmoon@gmail.com>
2024-03-05 17:16:54 -0500

    cast

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
+from os import path
+basedir = path.abspath(path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:@localhost/citas_2826502"
+    # SQLALCHEMY_DATABASE_URI="mysql+pymysql://root:@localhost/citas_2826502"
+    SQLALCHEMY_DATABASE_URI="sqlite:///" + path.join(basedir, 'database.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS=True
